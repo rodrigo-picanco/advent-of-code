1fd04733922b90d7b0d48edc76e472e51a08eb4b
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-12-08 20:22:33 +0100

    day 08 - part1

--- 2023/day_08/src/bin/part2.rs
+++ 2023/day_08/src/bin/part2.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 fn main() {
     let input = include_str!("./input1.txt");
     let output = process_input(input);
@@ -5,54 +7,139 @@ fn main() {
 }
 
 fn process_input(input: &str) -> usize {
-    input
-        .lines()
-        .map(|x| process_line(x))
-        .sum()
+    let (input, (moves, nodes)) = parse_input(input).expect("Failed to parse input");
+    assert_eq!(input, "");
+
+    let mut schema = Schema::new();
+    for (name, left, right) in nodes {
+        schema.insert(
+            name.clone(),
+            State::new(
+                name.clone(),
+                (left, right),
+                name.clone().pop().unwrap() == 'A',
+            ),
+        );
+    }
+
+   let keys =  schema
+        .keys()
+        .filter(|key| key.ends_with("A")).collect();
 }
 
-fn process_line(input: &str) -> usize {
-    let mut result = String::new();
-    let mut subarrays = Vec::new();
+type Schema = BTreeMap<String, State>;
 
-    for x in 0..input.len() {
-        for y in x..input.len() {
-            subarrays.push(&input[x..y + 1]);
+#[derive(Debug, PartialEq, Clone)]
+struct State {
+    name: String,
+    transitions: (String, String),
+    is_initial: bool,
+}
+impl State {
+    fn new(name: String, transitions: (String, String), is_initial: bool) -> Self {
+        Self {
+            name,
+            transitions,
+            is_initial,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+struct WalkMachine {
+    state: State,
+    schema: Schema,
+    steps: usize,
+}
+impl WalkMachine {
+    fn new(schema: Schema) -> Self {
+        let initial = schema.get("initial").expect("No initial state").clone();
+        Self {
+            state: schema.get(&initial.name).expect("No initial state").clone(),
+            schema,
+            steps: 0,
         }
     }
 
-    let mut buffer: Vec<char> = subarrays.iter().map(|x| map(x)).collect();
-
-    buffer = buffer
-        .iter()
-        .filter(|x| **x != '0')
-        .map(|x| *x as char)
-        .collect();
-
-    result.push(buffer[0]);
-    result.push(buffer.pop().unwrap());
-    result.parse::<usize>().unwrap()
-}
-
-fn map(input: &str) -> char {
-    match input {
-        "one" => '1',
-        "two" => '2',
-        "three" => '3',
-        "four" => '4',
-        "five" => '5',
-        "six" => '6',
-        "seven" => '7',
-        "eight" => '8',
-        "nine" => '9',
-        _ => {
-            if input.len() == 1 && input.chars().nth(0).unwrap().is_numeric() {
-                input.chars().nth(0).unwrap()
-            } else {
-                '0'
+    // TODO: Could we avoid cloning the state?
+    fn walk(&mut self, direction: Direction) {
+        self.steps += 1;
+        match direction {
+            Direction::Left => {
+                self.state = self
+                    .schema
+                    .get(&self.state.transitions.0)
+                    .expect("No state. Cannot move left")
+                    .clone();
+            }
+            Direction::Right => {
+                self.state = self
+                    .schema
+                    .get(&self.state.transitions.1)
+                    .expect("No state. Cannot move right")
+                    .clone();
             }
         }
     }
+}
+
+fn parse_move(input: &str) -> nom::IResult<&str, Direction> {
+    let (input, direction) = nom::branch::alt((
+        nom::bytes::complete::tag("L"),
+        nom::bytes::complete::tag("R"),
+    ))(input)?;
+    Ok((
+        input,
+        match direction {
+            "L" => Direction::Left,
+            "R" => Direction::Right,
+            _ => panic!("Invalid direction"),
+        },
+    ))
+}
+fn parse_name(input: &str) -> nom::IResult<&str, String> {
+    let (input, name) = nom::character::complete::alphanumeric1(input)?;
+    Ok((input, name.to_string()))
+}
+fn parse_transition(input: &str) -> nom::IResult<&str, (String, String)> {
+    nom::sequence::delimited(
+        nom::bytes::complete::tag("("),
+        nom::sequence::separated_pair(parse_name, nom::bytes::complete::tag(", "), parse_name),
+        nom::bytes::complete::tag(")"),
+    )(input)
+}
+fn parse_node(input: &str) -> nom::IResult<&str, (String, String, String)> {
+    let (input, (name, (left, right))) = nom::sequence::separated_pair(
+        parse_name,
+        nom::bytes::complete::tag(" = "),
+        parse_transition,
+    )(input)?;
+    Ok((input, (name, left, right)))
+}
+fn parse_nodes(input: &str) -> nom::IResult<&str, Vec<(String, String, String)>> {
+    let (input, output) = nom::multi::many1(nom::sequence::terminated(
+        parse_node,
+        nom::branch::alt((nom::character::complete::line_ending, nom::combinator::eof)),
+    ))(input)?;
+    Ok((input, output))
+}
+fn parse_moves(input: &str) -> nom::IResult<&str, Vec<Direction>> {
+    let (input, output) = nom::sequence::terminated(
+        nom::multi::many1(parse_move),
+        nom::multi::many1(nom::character::complete::line_ending),
+    )(input)?;
+    Ok((input, output))
+}
+fn parse_input(input: &str) -> nom::IResult<&str, (Vec<Direction>, Vec<(String, String, String)>)> {
+    let (input, moves) = parse_moves(input)?;
+    let (input, nodes) = parse_nodes(input)?;
+    Ok((input, (moves, nodes)))
 }
 
 #[cfg(test)]
@@ -60,13 +147,184 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_count_steps() {
+        let mut schema = Schema::new();
+        schema.insert(
+            "initial".to_string(),
+            State::new("AAA".to_string(), ("".to_string(), "".to_string()), true),
+        );
+        schema.insert(
+            "AAA".to_string(),
+            State::new(
+                "AAA".to_string(),
+                ("BBB".to_string(), "CCC".to_string()),
+                true,
+            ),
+        );
+        schema.insert(
+            "BBB".to_string(),
+            State::new(
+                "BBB".to_string(),
+                ("AAA".to_string(), "CCC".to_string()),
+                false,
+            ),
+        );
+
+        schema.insert(
+            "CCC".to_string(),
+            State::new(
+                "CCC".to_string(),
+                ("CCC".to_string(), "CCC".to_string()),
+                false,
+            ),
+        );
+        let mut machine = WalkMachine::new(schema);
+        assert_eq!(machine.steps, 0);
+
+        machine.walk(Direction::Left);
+        assert_eq!(machine.steps, 1);
+
+        machine.walk(Direction::Right);
+        assert_eq!(machine.steps, 2);
+    }
+
+    #[test]
+    fn test_walk() {
+        use Direction::*;
+        let mut schema = Schema::new();
+        schema.insert(
+            "initial".to_string(),
+            State::new("AAA".to_string(), ("".to_string(), "".to_string()), true),
+        );
+        schema.insert(
+            "AAA".to_string(),
+            State::new(
+                "AAA".to_string(),
+                ("BBB".to_string(), "BBB".to_string()),
+                true,
+            ),
+        );
+        schema.insert(
+            "BBB".to_string(),
+            State::new(
+                "BBB".to_string(),
+                ("AAA".to_string(), "CCC".to_string()),
+                false,
+            ),
+        );
+        schema.insert(
+            "CCC".to_string(),
+            State::new(
+                "CCC".to_string(),
+                ("CCC".to_string(), "CCC".to_string()),
+                false,
+            ),
+        );
+        let mut machine = WalkMachine::new(schema);
+
+        struct Test {
+            from: String,
+            to: String,
+            direction: Direction,
+        }
+        impl Test {
+            fn new(from: String, to: String, direction: Direction) -> Self {
+                Self {
+                    from,
+                    to,
+                    direction,
+                }
+            }
+        }
+
+        let tests = vec![
+            Test::new("AAA".to_string(), "BBB".to_string(), Left),
+            Test::new("BBB".to_string(), "AAA".to_string(), Left),
+            Test::new("AAA".to_string(), "BBB".to_string(), Right),
+            Test::new("BBB".to_string(), "AAA".to_string(), Left),
+            Test::new("AAA".to_string(), "BBB".to_string(), Left),
+            Test::new("BBB".to_string(), "CCC".to_string(), Right),
+        ];
+
+        for test in tests {
+            assert_eq!(machine.state.name, test.from);
+            machine.walk(test.direction);
+            assert_eq!(machine.state.name, test.to);
+        }
+    }
+
+    #[test]
+    fn test_parse_input() {
+        let input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+        let expected_nodes = [
+            ("AAA", "BBB", "CCC"),
+            ("BBB", "DDD", "EEE"),
+            ("CCC", "ZZZ", "GGG"),
+            ("DDD", "DDD", "DDD"),
+            ("EEE", "EEE", "EEE"),
+            ("GGG", "GGG", "GGG"),
+            ("ZZZ", "ZZZ", "ZZZ"),
+        ];
+        let (_, (moves, nodes)) = parse_input(input).unwrap();
+
+        assert_eq!(moves, vec![Direction::Right, Direction::Left]);
+
+        for x in 0..nodes.len() {
+            assert_eq!(
+                nodes[x],
+                (
+                    expected_nodes[x].0.to_string(),
+                    expected_nodes[x].1.to_string(),
+                    expected_nodes[x].2.to_string()
+                )
+            )
+        }
+    }
+
+    #[test]
+    fn test_create_machine() {
+        let mut schema = Schema::new();
+        schema.insert(
+            "initial".to_string(),
+            State::new("AAA".to_string(), ("".to_string(), "".to_string()), true),
+        );
+        schema.insert(
+            "AAA".to_string(),
+            State::new(
+                "AAA".to_string(),
+                ("BBB".to_string(), "CCC".to_string()),
+                true,
+            ),
+        );
+        let machine = WalkMachine::new(schema);
+        assert_eq!(machine.state.name, "AAA");
+    }
+
+    #[test]
     fn test_process() {
-        assert_eq!(process_line("two1nine"), 29);
-        assert_eq!(process_line("eightwothree"), 83);
-        assert_eq!(process_line("abcone2threexyz"), 13);
-        assert_eq!(process_line("xtwone3four"), 24);
-        assert_eq!(process_line("4nineeightseven2"), 42);
-        assert_eq!(process_line("zoneight234"), 14);
-        assert_eq!(process_line("7pqrstsixteen"), 76);
+        assert_eq!(
+            process_input(
+                "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"
+            ),
+            6
+        );
     }
 }
