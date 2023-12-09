9edd0858c2cac4de5b3233de21bddb05d97a4b2e
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-12-09 13:05:00 +0100

    day 09

---
+++ 2023/day_09/src/bin/part2.rs
@@ -0,0 +1,69 @@
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+
+fn extrapolate(input: Vec<isize>) -> isize {
+    if input.iter().all(|&x| x == 0) {
+        return 0;
+    }
+
+    let deltas = input.iter().skip(1).zip(input.iter()).map(|(a, b)| a - b).collect();
+
+    let diff = extrapolate(deltas);
+
+    input.first().expect("input is empty") - diff
+}
+
+fn process(input: &str) -> isize {
+    let input = parse(input);
+    input.iter().map(|row| extrapolate(row.clone())).sum()
+}
+
+fn parse(input: &str) -> Vec<Vec<isize>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse().expect("not a number"))
+                .collect()
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_input() {
+        assert_eq!(
+            parse(
+                "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
+            ),
+            vec![
+                vec![0, 3, 6, 9, 12, 15],
+                vec![1, 3, 6, 10, 15, 21],
+                vec![10, 13, 16, 21, 30, 45]
+            ]
+        )
+    }
+
+    #[test]
+    fn test_process() {
+        assert_eq!(
+            process(
+                "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"
+            ),
+           2 
+        );
+    }
+}
+
