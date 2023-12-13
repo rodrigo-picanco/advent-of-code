c974af30e451fb2711149871c4ef40b32d6c690a
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-12-13 19:43:52 +0100

    rename day_02 to day-02

--- 2023/day_02/target/debug/deps/part1-3c145265ae48a225.d
+++
@@ -1,6 +0,0 @@
-/Users/rodrigopicancotavares/Documents/projects/aoc/2023/day_02/target/debug/deps/part1-3c145265ae48a225.rmeta: src/bin/part1.rs src/bin/./input1.txt
-
-/Users/rodrigopicancotavares/Documents/projects/aoc/2023/day_02/target/debug/deps/part1-3c145265ae48a225.d: src/bin/part1.rs src/bin/./input1.txt
-
-src/bin/part1.rs:
-src/bin/./input1.txt:
