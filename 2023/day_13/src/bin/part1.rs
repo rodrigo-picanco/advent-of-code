82bc8dfdd31ebfdfc56b34d3c6874b69f5c0d687
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-12-13 13:11:47 +0100

    day 13

---
+++ 2023/day_13/src/bin/part1.rs
@@ -0,0 +1,102 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> usize {
+    input.split("\n\n").map(process_pattern).sum()
+}
+
+const HORIZONTAL_FACTOR: usize = 100;
+const VERTICAL_FACTOR: usize = 1;
+
+
+fn process_pattern(input: &str) -> usize {
+    let lines = input
+        .lines()
+        .map(|x| x.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let cols = (0..lines[0].len())
+        .map(|i| lines.iter().map(|x| x[i]).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let horizontal = find_mirror(lines);
+    let vertical = find_mirror(cols);
+
+    [(horizontal, HORIZONTAL_FACTOR), (vertical, VERTICAL_FACTOR)]
+        .iter()
+        .find_map(|(x, y)| if let Some(x) = x { Some(x * y) } else { None })
+        .expect("No pattern found")
+}
+
+fn find_mirror(input: Vec<Vec<char>>) -> Option<usize> {
+    input
+        .iter()
+        .enumerate()
+        .zip(input.iter().skip(1))
+        .filter_map(|((i, a), b)| if a == b { Some(i) } else { None })
+        .find_map(|x| {
+            let (first, second) = input.split_at(x + 1);
+
+            let second = second.iter().collect::<Vec<_>>();
+            let first = first.iter().rev().take(second.len()).collect::<Vec<_>>();
+
+            if first == second {
+                Some(x + 1)
+            } else {
+                None
+            }
+        })
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("\
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.", 5 ; "vertical")]
+    #[test_case("\
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#",  400  ; "horizontal")]
+    fn test_process_pattern(input: &str, output: usize) {
+        assert_eq!(process_pattern(input), output);
+    }
+
+    #[test]
+    fn test_process() {
+        assert_eq!(
+            process(
+                "\
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"
+            ),
+            405
+        );
+    }
+}
---
+++ 2023/day_13/src/bin/part2.rs
@@ -0,0 +1,103 @@
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn process(input: &str) -> usize {
+    input.split("\n\n").map(process_pattern).sum()
+}
+
+fn process_pattern(input: &str) -> usize {
+    let lines = input
+        .lines()
+        .map(|x| x.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let cols = (0..lines[0].len())
+        .map(|i| lines.iter().map(|x| x[i]).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let mut result = 0;
+    let horizontal = find_mirror(lines);
+    let vertical = find_mirror(cols);
+
+    result += horizontal * 100;
+    result += vertical;
+    result
+
+}
+
+fn find_mirror(input: Vec<Vec<char>>) -> usize {
+    for r in 0..input.len() {
+        let (first, second) = input.split_at(r);
+
+        let mut count = 0;
+        for (x, y) in first.iter().rev().zip(second) {
+            for (a, b) in x.iter().zip(y.iter()) {
+                if a != b {
+                    count += 1;
+                }
+            }
+        }
+
+        if count == 1 {
+            return r;
+        }
+    }
+
+    return 0;
+}
+
+// LOW 36500
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("\
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.", 300 ; "first")]
+    #[test_case("\
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#",  100  ; "second")]
+    fn test_process_pattern(input: &str, output: usize) {
+        assert_eq!(process_pattern(input), output);
+    }
+
+    #[test]
+    fn test_process() {
+        assert_eq!(
+            process(
+                "\
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#"
+            ),
+            400
+        );
+    }
+}
