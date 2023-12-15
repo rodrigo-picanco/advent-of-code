11045710fad8306e3a6e2b6d5da41f8949c96a71
Rodrigo Picanço Tavares <rodrigopicancotavares@gmail.com>
2023-12-15 10:15:17 +0100

    day 14

--- 2023/day-14/src/bin/part1.rs
+++ 2023/day-14/src/bin/part1.rs
@@ -5,63 +5,90 @@ fn main() {
 }
 
 fn process(input: &str) -> usize {
-    let grid = parse(input);
-    grid.cols()
-        .iter()
-        .map(|col| {
-            col.split(|tile| tile.char == '#')
-                .map(|group| {
-                    let empty = group.iter().filter(|tile| tile.char == '.').count();
-                    group
-                        .iter()
-                        .rev()
-                        .skip(empty)
-                        .map(|tile| tile.power)
-                        .sum::<usize>()
+    calc_north_load(string_to_grid(&tilt(input, Direction::North)))
+}
+
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+fn tilt(input: &str, direction: Direction) -> String {
+    use Direction::*;
+
+    match direction {
+        North | South => grid_to_string(rotate(
+            rotate(string_to_grid(input))
+                .into_iter()
+                .map(|col| {
+                    col.split(|x| x == &'#')
+                        .map(|group| group.to_vec())
+                        .map(|mut group| {
+                            group.sort();
+                            match direction {
+                                North => group.iter().rev().collect::<String>(),
+                                South => group.iter().collect::<String>(),
+                                _ => panic!("invalid direction"),
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .join("#")
                 })
-                .sum::<usize>()
-        })
+                .map(|line| line.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>(),
+        )),
+
+        East | West => grid_to_string(
+            string_to_grid(input)
+                .into_iter()
+                .map(|col| {
+                    col.split(|x| x == &'#')
+                        .map(|group| group.to_vec())
+                        .map(|mut group| {
+                            group.sort();
+                            match direction {
+                                East => group.iter().rev().collect::<String>(),
+                                West => group.iter().collect::<String>(),
+                                _ => panic!("invalid direction"),
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .join("#")
+                })
+                .map(|line| line.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>(),
+        ),
+    }
+}
+
+fn rotate(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    (0..input.len())
+        .map(|y| input.iter().map(move |line| line[y]).collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+}
+
+fn grid_to_string(grid: Vec<Vec<char>>) -> String {
+    grid.iter()
+        .map(|row| row.iter().collect::<String>())
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+fn string_to_grid(input: &str) -> Vec<Vec<char>> {
+    input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+}
+
+fn calc_north_load(input: Vec<Vec<char>>) -> usize {
+    input.iter()
+        .enumerate()
+        .map(|(r, row)| row.iter().filter(|x| **x == 'O').count() * (input.len() - r))
         .sum()
 }
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Tile {
-    power: usize,
-    char: char,
-}
-
-#[derive(Debug)]
-struct Grid {
-    tiles: Vec<Vec<Tile>>,
-    height: usize,
-}
-impl Grid {
-    fn cols(&self) -> Vec<Vec<Tile>> {
-        (0..self.height)
-            .map(|y| self.tiles.iter().map(|x| x[y]).collect())
-            .collect()
-    }
-}
-
-fn parse(input: &str) -> Grid {
-    let height = input.lines().count();
-    Grid {
-        tiles: input
-            .lines()
-            .enumerate()
-            .map(|(y, line)| {
-                line.chars()
-                    .map(|char| Tile {
-                        power: height - y,
-                        char,
-                    })
-                    .collect::<Vec<Tile>>()
-            })
-            .collect(),
-        height,
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
--- 2023/day-14/src/bin/part2.rs
+++ 2023/day-14/src/bin/part2.rs
@@ -5,7 +5,118 @@ fn main() {
 }
 
 fn process(input: &str) -> usize {
-    todo!()
+    let mut count = 0;
+    let mut result = input.to_string();
+    let mut seen = vec![result.clone()];
+
+    loop {
+        count += 1;
+        result = cycle(&result);
+        if seen.contains(&result) {
+            break;
+        }
+        seen.push(result.clone());
+    }
+
+    let first = seen.iter().position(|x| x == &result).expect("acc empty");
+
+    let pos = (1_000_000_000 - first) % (count - first) + first;
+
+    result = seen[pos].clone();
+
+    println!("{}", result);
+
+    calc_north_load(encode(&result))
+}
+
+fn cycle(input: &str) -> String {
+    let mut result = tilt(input, Direction::North);
+    result = tilt(&result, Direction::West);
+    result = tilt(&result, Direction::South);
+    result = tilt(&result, Direction::East);
+    result
+}
+
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+fn tilt(input: &str, direction: Direction) -> String {
+    use Direction::*;
+
+    match direction {
+        North | South => decode(rotate(
+            rotate(encode(input))
+                .into_iter()
+                .map(|col| {
+                    col.split(|x| x == &'#')
+                        .map(|group| group.to_vec())
+                        .map(|mut group| {
+                            group.sort();
+                            match direction {
+                                North => group.iter().rev().collect::<String>(),
+                                South => group.iter().collect::<String>(),
+                                _ => panic!("invalid direction"),
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .join("#")
+                })
+                .map(|line| line.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>(),
+        )),
+
+        East | West => decode(
+            encode(input)
+                .into_iter()
+                .map(|col| {
+                    col.split(|x| x == &'#')
+                        .map(|group| group.to_vec())
+                        .map(|mut group| {
+                            group.sort();
+                            match direction {
+                                East => group.iter().collect::<String>(),
+                                West => group.iter().rev().collect::<String>(),
+                                _ => panic!("invalid direction"),
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .join("#")
+                })
+                .map(|line| line.chars().collect::<Vec<_>>())
+                .collect::<Vec<_>>(),
+        ),
+    }
+}
+
+fn rotate(input: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    (0..input.len())
+        .map(|y| input.iter().map(move |line| line[y]).collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+}
+
+fn decode(grid: Vec<Vec<char>>) -> String {
+    grid.iter()
+        .map(|row| row.iter().collect::<String>())
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+fn encode(input: &str) -> Vec<Vec<char>> {
+    input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+}
+
+fn calc_north_load(input: Vec<Vec<char>>) -> usize {
+    input.iter()
+        .enumerate()
+        .map(|(r, row)| row.iter().filter(|x| **x == 'O').count() * (input.len() - r))
+        .sum()
 }
 
 #[cfg(test)]
@@ -27,7 +138,7 @@ O.#..O.#.#
 #....###..
 #OO..#...."
             ),
-            136
+            64
         );
     }
 }
